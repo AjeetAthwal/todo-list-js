3f68ce69059eebd7117924190b16c0f3841c83af
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-12-03 18:31:15 +0000

    refactored code and added sorting into Projects class (WIP)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import Projects from './modules/projects';
 
 const desc1 = "Create ToDo Class description";
 
-const myProjects = new Projects();
+const myProjects = new Projects("dueDateEarliestFirst", "dueDateEarliestFirst");
 
 myProjects.addProject("My Project", "lol", 4, new Date(2020,11,6))
 
@@ -25,58 +25,58 @@ console.log(myProjects);
 console.log(myProjects.getList());
 console.log(myProjects.getToDoList());
 
-function compareDueDate(first, second){
-    if (first.dueDate === second.dueDate) return 0;
-    if (first.dueDate === "") return 1;
-    if (second.dueDate === "") return -1;
-    if (first.dueDate < second.dueDate) return -1;
-    else return 1;
-}
+// function compareDueDate(first, second){
+//     if (first.dueDate === second.dueDate) return 0;
+//     if (first.dueDate === "") return 1;
+//     if (second.dueDate === "") return -1;
+//     if (first.dueDate < second.dueDate) return -1;
+//     else return 1;
+// }
 
-function comparePriority(first, second){
-    if (first.priority === second.priority) return 0;
-    if (first.priority < second.priority) return -1;
-    else return 1;
-}
+// function comparePriority(first, second){
+//     if (first.priority === second.priority) return 0;
+//     if (first.priority < second.priority) return -1;
+//     else return 1;
+// }
 
-function compareCreationDate(first, second){
-    if (first.creationDatetime === second.creationDatetime) return 0;
-    if (first.creationDatetime < second.creationDatetime) return -1;
-    else return 1;
-}
+// function compareCreationDate(first, second){
+//     if (first.creationDatetime === second.creationDatetime) return 0;
+//     if (first.creationDatetime < second.creationDatetime) return -1;
+//     else return 1;
+// }
 
-myProjects._list.sort(compareDueDate) // earliest first
+// myProjects._list.sort(compareDueDate) // earliest first
 
-console.log(myProjects);
-console.log(myProjects.getList());
-console.log(myProjects.getToDoList());
+// console.log(myProjects);
+// console.log(myProjects.getList());
+// console.log(myProjects.getToDoList());
 
-myProjects._list.sort(compareDueDate).reverse() // oldest first
+// myProjects._list.sort(compareDueDate).reverse() // oldest first
 
-console.log(myProjects);
-console.log(myProjects.getList());
-console.log(myProjects.getToDoList());
+// console.log(myProjects);
+// console.log(myProjects.getList());
+// console.log(myProjects.getToDoList());
 
-myProjects._list.sort(comparePriority)  // lower priority number first (ie highest priority)
+// myProjects._list.sort(comparePriority)  // lower priority number first (ie highest priority)
 
-console.log(myProjects);
-console.log(myProjects.getList());
-console.log(myProjects.getToDoList());
+// console.log(myProjects);
+// console.log(myProjects.getList());
+// console.log(myProjects.getToDoList());
 
-myProjects._list.sort(comparePriority).reverse()   // higher priority number first (ie lowest priority)
+// myProjects._list.sort(comparePriority).reverse()   // higher priority number first (ie lowest priority)
 
-console.log(myProjects);
-console.log(myProjects.getList());
-console.log(myProjects.getToDoList());
+// console.log(myProjects);
+// console.log(myProjects.getList());
+// console.log(myProjects.getToDoList());
 
-myProjects._list.sort(compareCreationDate) // oldest first
+// myProjects._list.sort(compareCreationDate) // oldest first
 
-console.log(myProjects);
-console.log(myProjects.getList());
-console.log(myProjects.getToDoList());
+// console.log(myProjects);
+// console.log(myProjects.getList());
+// console.log(myProjects.getToDoList());
 
-myProjects._list.sort(compareCreationDate).reverse() // newest first
+// myProjects._list.sort(compareCreationDate).reverse() // newest first
 
-console.log(myProjects);
-console.log(myProjects.getList());
-console.log(myProjects.getToDoList());
+// console.log(myProjects);
+// console.log(myProjects.getList());
+// console.log(myProjects.getToDoList());
