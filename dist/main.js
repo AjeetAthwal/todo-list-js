0435e66b609e07bf41431dd17952339abb04569f
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-12-03 11:24:49 +0000

    looking into sorting projects

--- dist/main.js
+++ dist/main.js
@@ -3058,9 +3058,6 @@ myProjects.addToDoToLatestProject("Hsadi", "ss", 2, new Date(2021));
 
 
 window.b = myProjects;
-console.log(myProjects);
-console.log(myProjects.getList());
-console.log(myProjects.getToDoList());
 
 myProjects.addToDoToProject("","Hsadi", "ss", 2, new Date(2021));
 
@@ -3068,19 +3065,57 @@ console.log(myProjects);
 console.log(myProjects.getList());
 console.log(myProjects.getToDoList());
 
-myProjects.removeToDoFromProject(0,1);
+function compareDueDate(first, second){
+    if (first.dueDate === second.dueDate) return 0;
+    if (first.dueDate === "") return 1;
+    if (second.dueDate === "") return -1;
+    if (first.dueDate < second.dueDate) return -1;
+    else return 1;
+}
+
+function comparePriority(first, second){
+    if (first.priority === second.priority) return 0;
+    if (first.priority < second.priority) return -1;
+    else return 1;
+}
+
+function compareCreationDate(first, second){
+    if (first.creationDatetime === second.creationDatetime) return 0;
+    if (first.creationDatetime < second.creationDatetime) return -1;
+    else return 1;
+}
+
+myProjects._list.sort(compareDueDate) // earliest first
 
 console.log(myProjects);
 console.log(myProjects.getList());
 console.log(myProjects.getToDoList());
 
-myProjects.removeProject(1);
+myProjects._list.sort(compareDueDate).reverse() // oldest first
 
 console.log(myProjects);
 console.log(myProjects.getList());
 console.log(myProjects.getToDoList());
 
-myProjects.removeProject(2);
+myProjects._list.sort(comparePriority)  // lower priority number first (ie highest priority)
+
+console.log(myProjects);
+console.log(myProjects.getList());
+console.log(myProjects.getToDoList());
+
+myProjects._list.sort(comparePriority).reverse()   // higher priority number first (ie lowest priority)
+
+console.log(myProjects);
+console.log(myProjects.getList());
+console.log(myProjects.getToDoList());
+
+myProjects._list.sort(compareCreationDate) // oldest first
+
+console.log(myProjects);
+console.log(myProjects.getList());
+console.log(myProjects.getToDoList());
+
+myProjects._list.sort(compareCreationDate).reverse() // newest first
 
 console.log(myProjects);
 console.log(myProjects.getList());
