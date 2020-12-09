acf42196173ed7f60c56e4ac0a8098a6e49bc819
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-12-09 20:07:18 +0000

    refactored to include less magic literals

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import {ProjectsStorage, SettingsStorage} from './modules/storage';
 import {defaultProjectsEntry, defaultSettingsEntry} from './modules/defaultEntry'
 import {NewCardLoader, ProjectCardsLoader, CardsLoader, SortFormLoader, TasksPageLoader, DisplayController} from './modules/displayController'
 
-const mySettingsStorage = new SettingsStorage(false, true, defaultSettingsEntry);
-const myProjectsStorage = new ProjectsStorage(false, true, defaultProjectsEntry);
+const serverStorageExists = false;
+const localStorageExists = true;
+
+const mySettingsStorage = new SettingsStorage(serverStorageExists, localStorageExists, defaultSettingsEntry);
+const myProjectsStorage = new ProjectsStorage(serverStorageExists, localStorageExists, defaultProjectsEntry);
+
 const mySettings = new Settings(mySettingsStorage, "", "", "", "");
 const myProjects = new Projects(myProjectsStorage, mySettings);
 const myToDos = new ToDos(mySettings, myProjects)
@@ -17,6 +21,9 @@ console.log(myToDos.getList());
 const newCardLoader = new NewCardLoader(myProjects)
 const projectCardsLoader = new ProjectCardsLoader(myProjects)
 const cardsLoader = new CardsLoader(myProjects, newCardLoader, projectCardsLoader)
+
 const sortFormLoader = new SortFormLoader(myProjects)
+
 const tasksPageLoader = new TasksPageLoader(myProjects, sortFormLoader, cardsLoader)
+
 const displayController = new DisplayController(tasksPageLoader)
